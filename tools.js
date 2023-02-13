b0617effb12e36ab24ab869f5db9b8c5241b3e3d
xyz <81219723@qq.com>
2023-02-13 17:57:59 +0800

    add 07_scrollPages

---
+++ tools.js
@@ -0,0 +1,60 @@
+$(function() {
+	var $outer = $('#outer')
+	var $imgList = $('#imgList')
+	var $imgs = $('img')
+	var $navDiv = $('#navDiv')
+	var $allA = $('a')
+
+	var index = 0
+	var PAGE_WIDTH = 520
+	var TIME = 400
+	var INTERVAL = 20
+
+	$imgList.css('width', PAGE_WIDTH * $imgs.length)
+	var x = $outer.offset().left
+	var y = $navDiv.offset().left
+	$navDiv.css('left', ($outer.width() - $navDiv.width()) / 2)
+	// $allA.css('background', 'black')
+
+	$allA.each(function(i) {
+		this.i = i
+	})
+
+	$navDiv.delegate('a', 'click', function() {
+		if (index != this.i) {
+			index = this.i
+			updatePage()
+		}
+	})
+
+	setInterval(function() {
+		console.log(index)
+		updatePage(function() {
+			if (index >= $imgs.length - 1) {
+				index = 0;
+				imgList.style.left = 0;
+			}
+		})
+		index++
+		index %= $imgs.length
+	}, 1000)
+
+	function updatePage(callback) {
+		var currLeft = $imgList.position().left
+		var i = Math.abs(index - (currLeft / PAGE_WIDTH))
+		var offset = -PAGE_WIDTH * Math.ceil(i)
+		var itemOffset = offset / (TIME / INTERVAL)
+		var target = currLeft + offset
+
+
+		var intervalId = setInterval(function() {
+			currLeft += itemOffset
+			$imgList.css('left', currLeft)
+
+			if (currLeft === target) {
+				clearInterval(intervalId)
+				callback && callback()
+			}
+		}, INTERVAL)
+	}
+})
