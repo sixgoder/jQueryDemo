5bee1848ef6c39966a376e5961a5d1f10feaf416
xyz <81219723@qq.com>
2023-02-15 11:21:03 +0800

    add 08_extand

---
+++ jq_extend.js
@@ -0,0 +1,32 @@
+(function() {
+	// jQuery对象扩展
+	$.extend({
+		min: function(a, b) {
+			return a < b ? a : b
+		},
+		max: function(a, b) {
+			return a > b ? a : b
+		},
+		leftTrim: function(str) {
+			return str.replace(/^\s+/, '')
+		},
+		rightTrim: function(str) {
+			return str.replace(/\s+$/, '')
+		}
+	})
+
+	//jQuery方法扩展
+	$.fn.extend({
+		checkAll: function() {
+			this.prop('checked', true)
+		},
+		unCheckAll: function() {
+			this.prop('checked', false)
+		},
+		reverseCheck: function() {
+			this.each(function() {
+				this.checked = !this.checked
+			})
+		}
+	})
+})()
